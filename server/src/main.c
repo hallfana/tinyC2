bdf5b5273d1613c6ff21bb7c4cf3bb83222551a3
hallfana <hallfana@proton.me>
2024-12-20 04:50:40 +0100

    dev

--- server/src/main.c
+++ server/src/main.c
@@ -6,7 +6,7 @@
 /*   By: hallfana <hallfana@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/19 17:37:28 by hallfana          #+#    #+#             */
-/*   Updated: 2024/12/20 04:48:12 by hallfana         ###   ########.fr       */
+/*   Updated: 2024/12/20 04:50:38 by hallfana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ int main(void)
 {
 	t_server server;
 	_tc_init_server(&server);
+	_tc_init_listener(&server);
 	_tc_clean_exit(&server, 0);
 	return (0);
 }
